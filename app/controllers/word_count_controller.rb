824acbaf1711ef502ae1d4d3d26833c1581220b2
Rich Gehlmann <rgehlma0@chicagobooth.edu>
2019-02-01 17:39:50 +0000

    Word Count Complete

--- app/controllers/word_count_controller.rb
+++ app/controllers/word_count_controller.rb
@@ -2,20 +2,22 @@ class WordCountController < ApplicationController
   def word_count
     @text = params.fetch("user_text")
     @special_word = params.fetch("user_word")
-
+  
+   
+    
     # ================================================================================
     # Your code goes below.
     # The text the user input is in the string @text.
     # The special word the user input is in the string @special_word.
     # ================================================================================
 
-    @word_count = "Replace this string with your answer"
+    @word_count = @text.split.count
 
-    @character_count_with_spaces = "Replace this string with your answer"
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer"
+    @character_count_without_spaces = @text.gsub(/\s+/, "").length
 
-    @occurrences = "Replace this string with your answer"
+    @occurrences = @special_word.split.count
 
     # ================================================================================
     # Your code goes above.
