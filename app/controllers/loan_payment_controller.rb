f999002479fd6e3b87f541d49afa13e8975816ff
Rich Gehlmann <rgehlma0@chicagobooth.edu>
2019-02-01 17:48:00 +0000

    loan payment

--- app/controllers/loan_payment_controller.rb
+++ app/controllers/loan_payment_controller.rb
@@ -3,6 +3,10 @@ class LoanPaymentController < ApplicationController
     @apr = params.fetch("annual_percentage_rate").to_f
     @years = params.fetch("number_of_years").to_i
     @principal = params.fetch("principal_value").to_f
+@rate=@apr/1200
+@months=@years*-12
+@denominator=1-(1+@rate)**@months
+@payment=@rate*@principal/@denominator
 
     # ================================================================================
     # Your code goes below.
@@ -11,7 +15,7 @@ class LoanPaymentController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer"
+    @monthly_payment = + @payment.to_s
 
     # ================================================================================
     # Your code goes above.
@@ -24,3 +28,12 @@ class LoanPaymentController < ApplicationController
     render("loan_payment_templates/loan_payment_form.html.erb")
   end
 end
+
+
+# apr=apr.to_f
+# years=years.chomp.to_i
+# balance=balance.chomp.to_f
+# rate=apr/1200
+# months=years*-12
+# denominator=1-(1+rate)**months
+# payment=rate*balance/denominator
